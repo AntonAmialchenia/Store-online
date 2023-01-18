3c3af112cc1c941d31bac33b9b5786b7c5ddd8f0
Антон Амельченя <107065275+AntonAmialchenia@users.noreply.github.com>
2023-01-18 13:50:48 +0300

    Обработка ошибок

--- server/error/ApiError.js
+++ server/error/ApiError.js
@@ -6,14 +6,16 @@ class ApiError extends Error{
     }
 
     static badRequest(message) {
-        return new ApiError(404, messages)
+        return new ApiError(404, message)
     }
 
     static internal(message) {
-        return new ApiError(500, messages)
+        return new ApiError(500, message)
     }
 
     static forbidden(message) {
-        return new ApiError(403, messages)
+        return new ApiError(403, message)
     }
 }
+
+module.exports = ApiError
