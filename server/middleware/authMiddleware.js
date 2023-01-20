e5901d7c301854c2f7fcc989ce7df575d1cf44e1
Антон Амельченя <107065275+AntonAmialchenia@users.noreply.github.com>
2023-01-20 20:15:22 +0300

    Авторизация
    Авторизация по токену

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function (req, res, next) {
+    if (req.method === 'OPTIONS') {
+        next()
+    }
+    try {
+        const token = req.headers.authorization.split(' ')[1] // из хедера достаем токен, так как сначала идет тип токена, разделяем его и по индексу получаем сам токен
+        if (!token) {
+           return res.status(401).json({message: 'Не авторизован'})
+        }
+        const decoded = jwt.verify(token, process.env.SECRET_KEY)
+        req.user = decoded
+        next()
+    } catch (e) {
+        res.status(401).json({message: 'Не авторизован'})
+    }
+}
