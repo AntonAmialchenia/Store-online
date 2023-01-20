9963920c00258aea57002614be6749e3dc6b8458
Антон Амельченя <107065275+AntonAmialchenia@users.noreply.github.com>
2023-01-20 19:18:45 +0300

    Регистрация
    Регистрация пользователя
    Использование jwt

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,21 +1,44 @@
-const ApiError = require('../error/ApiError')
+const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { User, Basket } = require('../models/models');
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: '24h',
+  });
+};
 
 class UserController {
-    async registration(req,res) {
-
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+    console.log(email, password, role);
+    if (!email || !password) {
+      return next(ApiError.badRequest(' Некорректный email или password'));
     }
-
-    async login(req,res) {
-        
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(
+        ApiError.badRequest('Ползователь с таким email не существует')
+      );
     }
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userID: user.id });
+    const token = generateJwt(user.id, user.email, user.role);
 
-    async check(req,res, next) {
-        const {id} = req.query
-        if (!id) {
-            return next(ApiError.badRequest('Не задан ID'))
-        }
-        res.json(id)
+    return res.json({ token });
+  }
+
+  async login(req, res) {}
+
+  async check(req, res, next) {
+    const { id } = req.query;
+    if (!id) {
+      return next(ApiError.badRequest('Не задан ID'));
     }
+    res.json(id);
+  }
 }
 
-module.exports = new UserController()
+module.exports = new UserController();
