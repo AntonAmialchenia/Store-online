56f2aeb86199b7cf03a2826ada7a41fa7019729c
Антон Амельченя <107065275+AntonAmialchenia@users.noreply.github.com>
2023-01-23 15:35:20 +0300

    Сделал страницу регистрации

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,31 +1,35 @@
 import { observer } from 'mobx-react-lite';
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/esm/Button';
 import { NavLink } from 'react-router-dom';
 import { SHOP_ROUTE } from '../utils/consts';
 import { Context } from './../index';
 
-
 const NavBar = () => {
-    const {user} = useContext(Context)
-    return (
-        <Navbar bg="primary" variant="dark">
-        <Container>
-          <NavLink style={{color:'white', textDecoration:'none'}} to={SHOP_ROUTE}>Купи Девайс</NavLink>
-          {user.isAuth ?
-            <Nav className="ml-auto">
-                <Button >Админ панель</Button>
-                <Button style={{marginLeft: 10}}>Войти</Button>
-            </Nav>
-          :
-            <Nav className="ml-auto">
-                <Button onClick={() => user.setIsAuth(true)}>Авторизация</Button>
-                
-            </Nav>}
-        </Container>
-      </Navbar>
-    );
+  const { user } = useContext(Context);
+  return (
+    <Navbar bg="primary" variant="dark">
+      <Container>
+        <NavLink
+          style={{ color: 'white', textDecoration: 'none' }}
+          to={SHOP_ROUTE}
+        >
+          Купи Девайс
+        </NavLink>
+        {user.isAuth ? (
+          <Nav className="ml-auto">
+            <Button>Админ панель</Button>
+            <Button style={{ marginLeft: 10 }}>Войти</Button>
+          </Nav>
+        ) : (
+          <Nav className="ml-auto">
+            <Button onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+          </Nav>
+        )}
+      </Container>
+    </Navbar>
+  );
 };
 
 export default observer(NavBar);
--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import { Container, Form } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/esm/Button';
+import Row from 'react-bootstrap/esm/Row';
+import { NavLink, useLocation } from 'react-router-dom';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from './../utils/consts';
+
 
 const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
+    console.log(location);
+    
     return (
-        <div>
-            Auth
-        </div>
+        <Container 
+        className="d-flex justify-content-center align-items-center"
+        style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
+                <Form className='d-flex flex-column'>
+                    <Form.Control
+                        className='mt-3'
+                        placeholder='Введите ваш email...'
+                    />
+                    <Form.Control
+                        className='mt-3'
+                        placeholder='Введите ваш пароль...'
+                    />
+                    <div className='d-flex justify-content-between mt-3 pl-3 pr-3 align-items-center'>
+                        {isLogin ? 
+                            <div>
+                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйтесь!</NavLink>
+                            </div>
+                            :
+                            <div>
+                                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                            </div>
+                        }
+                        <Button >
+                        {isLogin ? "Войти" : "Регистрация"}
+                        </Button>
+                    </div>
+                    
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
